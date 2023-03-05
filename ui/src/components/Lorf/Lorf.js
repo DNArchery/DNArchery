579129590ec99ec9eb3e5dc89d9e4d1c5caa836c
Vivek R <123vivekr@gmail.com>
2023-03-05 15:04:03 +0530

    UI: fix css

--- ui/src/components/Lorf/Lorf.js
+++ ui/src/components/Lorf/Lorf.js
@@ -3,12 +3,11 @@ import React from 'react'
 const Lorf = ({ lorf }) => {
     return (
         <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-            <div className='amono-acid'>
+            <div style={{ flex: 3 }} />
             <b>Lorf (Longest Open Reading Frame)</b><br/>
                 <div className="lorf">
                     {lorf}
                 </div>
-            </div>
         </div>
     )
 }
--- ui/src/components/Protein/Protein.js
+++ ui/src/components/Protein/Protein.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 const Protein = ({ protein }) => {
     return (
-        <div style={{ flex: 1, display: 'flex', flexDirection: 'column'}}>
-            <div className='amino-acid'>
+        <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+            <div style={{ flex: 3 }} />
             <b>Protein</b><br/>
-                <div className="lorf">
+                <div className="protein">
                     {protein}
-                </div>
             </div>
         </div>
     )

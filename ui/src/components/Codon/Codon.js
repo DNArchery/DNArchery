f1f1a0b2f7c81711c28ce371707bbf55ac48ce30
Vivek R <123vivekr@gmail.com>
2023-03-05 13:30:26 +0530

    UI: Parse Cordon Data

---
+++ ui/src/components/Codon/Codon.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const CodonCells = ({ label }) => {
+    return <div className='card'>
+        <p>{label}</p>
+    </div>
+}
+
+const Codon = ({ codon }) => {
+    return (
+        <div>
+                <table>
+                    {
+                        codon.forEach(group => {
+                            console.log(group);
+                            <tr>
+                                {
+                                    group.forEach(codon => {
+                                        return <td> { codon } </td>
+                                    })}
+                            </tr>
+                        })
+                    }
+                </table>
+        </div>
+    )
+}
+
+export default Codon;

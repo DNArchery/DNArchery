696732eea45cef2377e4e608692bd7a5c2cdd15e
Vivek R <123vivekr@gmail.com>
2023-03-05 14:02:52 +0530

    UI: Add LORF component

--- ui/src/screens/DNA.js
+++ ui/src/screens/DNA.js
@@ -3,12 +3,14 @@ import Input from '../components/Input/Input';
 import './DNA.css';
 import AminoAcid from '../components/AminoAcid/AminoAcid';
 import Codon from '../components/Codon/Codon';
+import Lorf from '../components/Lorf/Lorf';
 
 const DNA = (props) => {
 
     const { state, onChange } = props;
     const [aminoAcid, setAminoAcid] = React.useState(state.aminoAcid || [])
     const [codon, setCodon] = React.useState(state.codon || [])
+    const [lorf, setLorf] = React.useState(state.lorf|| [])
 
     React.useEffect(() => {
         onChange({ aminoAcid });
@@ -40,14 +42,31 @@ const DNA = (props) => {
             })
             .then(data => setCodon(data))
             .catch(error => console.error(error));
+
+        fetch('http://127.0.0.1:1337/sequence/lorf', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                sequence: query
+            })
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then(data => setLorf(data.lorf))
+            .catch(error => console.error(error));
+
     }
 
     return <div style={{ flex: 1, display: 'flex' }}>
         <div style={{ flex: 1 }} />
         <Input onSubmit={query => submit(query)} />
         <div className='output'>
-            {/* {aminoAcid.length > 0 ? <AminoAcid aminoAcid={aminoAcid} /> : null} */}
-            {codon.length > 0 ? <Codon codon={codon} /> : null}
+            {lorf.length > 0 ? <Lorf lorf={lorf} /> : null}
+            {codon.length > 0 ? <Codon codon={codon} /> : null} <br/>
+            {aminoAcid.length > 0 ? <AminoAcid aminoAcid={aminoAcid} /> : null}
         </div>
         <div style={{ flex: 1 }} />
     </div>
